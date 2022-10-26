661463d5aff50377d44ab2592d6b2771d08228f9
jiale_tan <jiale_tan@apple.com>
2022-10-26 00:45:16 -0700

    feat(dev-setup/setup-ozone-s3.sh): support local setup on MacOSX with arm64 architecture

--- dev-setup/setup-ozone-s3.sh
+++ dev-setup/setup-ozone-s3.sh
@@ -5,5 +5,15 @@
 
 # Create namespace to deploy Ozone
 kubectl create namespace local-ozone | echo "local-ozone namespace already exists. Ok..."
+
+# Replace Ozone docker image for MacOSX running on ARM64 architecture
+if [ "$(uname -s)" = "Darwin" ] && [ "$(arch)" = "arm64" ]; then
+  X86_IMAGE="apache/ozone:1.2.1"
+  AMD64_IMAGE="leletan/ozone:1.2.1-20220623-1-aarch64"
+  rm ozone/*.bak
+  find ozone/*-statefulset.yaml -type f -exec sed -i '.bak' "s~${X86_IMAGE}~${AMD64_IMAGE}~g" {} \;
+  rm ozone/*.bak
+fi
+
 # Deploy Ozone resources
 kubectl -n local-ozone apply -f ozone
