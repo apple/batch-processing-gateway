25031644f5d5e3cbff6abaf1d010bd8a66e7f422
Hai Tao <hai_tao@apple.com>
2023-05-10 18:37:34 -0700

    Correct the narrativeIssuer key name

--- src/main/java/com/apple/spark/appleinternal/WhisperConstants.java
+++ src/main/java/com/apple/spark/appleinternal/WhisperConstants.java
@@ -4,7 +4,7 @@ public class WhisperConstants {
     public static final String WHISPER_BUCKET_KEY_1 = "com.apple.whisper.bucket";
     public static final String WHISPER_BUCKET_KEY_2 = "whisper.apple.com/bucket";
     public static final String WHISPER_HYDROGEN_USER_KEY = "com.apple.whisper.hydrogen.appleConnectUser";
-    public static final String WHISPER_ISSUERNAME_KEY = "com.apple.whisper.issuerName";
+    public static final String WHISPER_ISSUERNAME_KEY = "whisper.apple.com/narrativeIssuer";
     public static final String WHISPER_ISSUERNAME = "spark-gateway";
     public static final String WHISPER_NAMESPACE_KEY = "com.apple.whisper.namespace";
     public static final String WHISPER_IDENTITY_TYPE_KEY = "com.apple.whisper.identityType";
