0b237957c534426d2eecbffcb250bd34e6449056
Claudia Sun <claudia_sun@apple.com>
2023-09-28 15:49:23 -0700

    modify ranger spark queue service name (#351)

--- src/main/bash/ranger/server/create-ranger-service.sh
+++ src/main/bash/ranger/server/create-ranger-service.sh
@@ -10,8 +10,8 @@ SERVICE_NAME=$4
 #
 # Remove existing service instances and servicedef
 #
-curl -v -i -L -k -u admin:${RANGER_ADMIN_PASSWORD} -H "Content-type: application/json" -X DELETE $RANGER_URL/service/public/v2/api/service/name/${SERVICE_NAME}
-curl -v -i -L -k -u admin:${RANGER_ADMIN_PASSWORD} -H "Content-type: application/json" -X DELETE $RANGER_URL/service/public/v2/api/servicedef/name/${SERVICE_TYPE}
+# curl -v -i -L -k -u admin:${RANGER_ADMIN_PASSWORD} -H "Content-type: application/json" -X DELETE $RANGER_URL/service/public/v2/api/service/name/${SERVICE_NAME}
+# curl -v -i -L -k -u admin:${RANGER_ADMIN_PASSWORD} -H "Content-type: application/json" -X DELETE $RANGER_URL/service/public/v2/api/servicedef/name/${SERVICE_TYPE}
 
 echo "Add ${SERVICE_TYPE} Service Def"
 curl -v -i -L -k -u admin:${RANGER_ADMIN_PASSWORD} -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d@./ranger-servicedef-${SERVICE_TYPE}.json $RANGER_URL/service/public/v2/api/servicedef
