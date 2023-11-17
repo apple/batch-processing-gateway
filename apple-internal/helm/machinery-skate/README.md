c2a8430ca8de72a198d98446d98c62a5b7a71660
Mingcui Yang <mingcui_yang@apple.com>
2023-11-17 08:05:52 -0800

    Add Machinery skate chart (#378)
    * add machinery chart and pipeline for the chart
    
    * Add empty line to the end of the file
    
    * Revert "Add empty line to the end of the file"
    
    This reverts commit e9639914e6d282b9cddc02e8b7e9bdd661230428.
    
    * clean changelog
    
    * update readme

---
+++ apple-internal/helm/machinery-skate/README.md
@@ -0,0 +1,3 @@
+# Batch Processing Gateway Machinery Helm Chart
+
+This is a variant of the Skate Helm chart modified to work with Machinery.
---
+++ apple-internal/machinery_publish_chart.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+re="skate-[0-9]+.[0-9]+.[0-9]+"
+dev_re="skate-dev-[0-9]+.[0-9]+.[0-9]+"
+
+if [[ "$1" =~ $re ]] || [[ "$1" =~ $dev_re ]]; then
+   SKATE_VERSION=$1
+else
+   echo "A skate tag must be provided."
+   exit 1
+fi
+
+HELM_CHART=${2:-apple-internal/helm/machinery-skate}
+HELM_REPO=${3:-apple-aiml-skate-machinery}
+IMG_REPO=${4:-aiml-di-dpi/skate/master}
+HELM_OUT=".out"
+HELM_VERSION="3.12.1"
+
+mkdir -p "${HELM_OUT}/${HELM_REPO}"
+wget -qO- https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz | tar xvz
+mv linux-amd64/helm /usr/bin/helm
+
+sed -E -i -e "s/(tag:[[:space:]])'skate-[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+'/\1${SKATE_VERSION}/g" ${HELM_CHART}/values.yaml
+sed -E -i -e "s/(repository:[[:space:]])'aiml-di-dpi\/skate\/master'/\1'${IMG_REPO//\//\\/}'/g" ${HELM_CHART}/values.yaml
+helm package --destination=${HELM_OUT}/${HELM_REPO} --version=${SKATE_VERSION##*-} ${HELM_CHART}
+
+ci stage-lib "${HELM_OUT}/**"
